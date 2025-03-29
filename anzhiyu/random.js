5d8061fafbfba0383b59c2e5c2cf0c12b2ece35a
YYY887 <ymy100861@qq.com>
2025-03-29 15:32:25 +0800

    博客更新: YYYY-03-2025年3月29日 15:32:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/29/hello-world/"];function toRandomPost(){
+var posts=["2025/03/29/hello-world/","2025/03/29/你的文章标题/","2025/03/29/你的文章标题-1/","2023/11/15/技术文章示例/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
